e36d0451fbc8c894412589b946e3ac11932ee0fd
Waldemar <dnestrgroup@gmail.com>
2024-03-18 10:47:32 +0200

    add to tests None annotation

--- app/tests/test_1_menu.py
+++ app/tests/test_1_menu.py
@@ -6,101 +6,101 @@ from app.tests.conftest import TestClientBase
 class TestMenu(TestClientBase):
     i = None
 
-    @pytest.mark.asyncio
-    async def test_clear_db(self):
+    @pytest.mark.asyncio  # type: ignore
+    async def test_clear_db(self) -> None:
         """
         чистим бд
         """
-        response = await self.client.get("/menus")
+        response = await self.client.get('/menus')
         for menu in response.json():
             await self.client.delete(f"/menus/{menu['id']}")
 
-    @pytest.mark.asyncio
-    async def test_get_menu(self):
+    @pytest.mark.asyncio  # type: ignore
+    async def test_get_menu(self) -> None:
         """
         проверяем что нет созданных меню
         """
-        response = await self.client.get("/menus")
+        response = await self.client.get('/menus')
         assert response.status_code == 200
         assert response.json() == []
 
-    @pytest.mark.asyncio
-    async def test_post_menu(self):
+    @pytest.mark.asyncio  # type: ignore
+    async def test_post_menu(self) -> None:
         """
         создаем пункт главного меню
         и проверяем ответ что пункт меню создан
         """
-        menu_title = "Drinks"
-        menu_description = "any description"
+        menu_title = 'Drinks'
+        menu_description = 'any description'
         data = {
-            "title": menu_title,
-            "description": menu_description,
+            'title': menu_title,
+            'description': menu_description,
         }
-        response = await self.client.post("/menus", json=data)
+        response = await self.client.post('/menus', json=data)
         assert response.status_code == 201
-        pytest.id_menu = response.json()["id"]
+        pytest.id_menu = response.json()['id']
         pytest.menu_title = menu_title
         pytest.menu_description = menu_description
-        assert response.json()["title"] == menu_title
-        assert response.json()["description"] == menu_description
-        assert response.json()["submenus_count"] == None
-        assert response.json()["dishes_count"] == None
+        assert response.json()['title'] == menu_title
+        assert response.json()['description'] == menu_description
+        assert response.json()['submenus_count'] == None  # noqa
+        assert response.json()['dishes_count'] == None  # noqa
 
-    @pytest.mark.asyncio(depends=["TestMenu::test_post_menu"])
-    async def test_get_menu_by_id(self):
+    @pytest.mark.asyncio(depends=['TestMenu::test_post_menu'])  # type: ignore
+    async def test_get_menu_by_id(self) -> None:
         """
         проверяем получение пункта меню по id
         """
-        response = await self.client.get(f"/menus/{pytest.id_menu}")
+        response = await self.client.get(f'/menus/{pytest.id_menu}')
         assert response.status_code == 200
-        assert response.json()["title"] == pytest.menu_title
-        assert response.json()["description"] == pytest.menu_description
-        assert response.json()["submenus_count"] == 0
-        assert response.json()["dishes_count"] == 0
+        assert response.json()['title'] == pytest.menu_title
+        assert response.json()['description'] == pytest.menu_description
+        assert response.json()['submenus_count'] == 0
+        assert response.json()['dishes_count'] == 0
 
-    @pytest.mark.asyncio
-    async def test_get_menu_not_empty(self):
+    @pytest.mark.asyncio  # type: ignore
+    async def test_get_menu_not_empty(self) -> None:
         """
         проверяем что меню не пустое
         """
-        response = await self.client.get("/menus")
+        response = await self.client.get('/menus')
         assert response.status_code == 200
         assert response.json() != []
 
-    @pytest.mark.asyncio(depends=["TestMenu::test_post_menu"])
-    async def test_update_menu_by_id(self):
+    @pytest.mark.asyncio(depends=['TestMenu::test_post_menu'])  # type: ignore
+    async def test_update_menu_by_id(self) -> None:
         """
         1. обновляем ранее созданное меню
         2. просматриваем только что созданное меню, что title description изменился на новое
         """
-        new_menu_title = "updated Drinks"
-        new_menu_description = "updated description"
+        new_menu_title = 'updated Drinks'
+        new_menu_description = 'updated description'
         data = {
-            "title": new_menu_title,
-            "description": new_menu_description,
+            'title': new_menu_title,
+            'description': new_menu_description,
         }
-        response = await self.client.patch(f"/menus/{pytest.id_menu}", json=data)
+        response = await self.client.patch(f'/menus/{pytest.id_menu}', json=data)
         assert response.status_code == 200
-        assert response.json()["title"] == new_menu_title
-        assert response.json()["description"] == new_menu_description
-        response = await self.client.get(f"/menus/{pytest.id_menu}")
+        assert response.json()['title'] == new_menu_title
+        assert response.json()['description'] == new_menu_description
+        response = await self.client.get(f'/menus/{pytest.id_menu}')
         assert response.status_code == 200
-        assert response.json()["title"] == new_menu_title
-        assert response.json()["description"] == new_menu_description
-        assert response.json()["submenus_count"] == 0
-        assert response.json()["dishes_count"] == 0
+        assert response.json()['title'] == new_menu_title
+        assert response.json()['description'] == new_menu_description
+        assert response.json()['submenus_count'] == 0
+        assert response.json()['dishes_count'] == 0
 
-    @pytest.mark.asyncio(depends=["TestMenu::test_post_menu"])
-    async def test_delete_menu_by_id(self):
+    @pytest.mark.asyncio(depends=['TestMenu::test_post_menu'])  # type: ignore
+    async def test_delete_menu_by_id(self) -> None:
         """
         1. удаляем обновленное меню
         2. просматриваем список меню
         3. просматриваем определенное меню и убеждаемся что его нет 404
         """
-        response = await self.client.delete(f"/menus/{pytest.id_menu}")
+        response = await self.client.delete(f'/menus/{pytest.id_menu}')
         assert response.status_code == 200
-        response = await self.client.get(f"/menus")
+        response = await self.client.get('/menus')
         assert response.status_code == 200
         assert response.json() == []
-        response = await self.client.get(f"/menus/{pytest.id_menu}")
+        response = await self.client.get(f'/menus/{pytest.id_menu}')
         assert response.status_code == 404
--- app/tests/test_2_submenu.py
+++ app/tests/test_2_submenu.py
@@ -5,137 +5,124 @@ from app.tests.conftest import TestClientBase
 
 class TestSubMenu(TestClientBase):
 
-    @pytest.mark.asyncio
-    async def test_post_menu(self):
+    @pytest.mark.asyncio  # type: ignore
+    async def test_post_menu(self) -> None:
         """
         создаем пункт главного меню
         и проверяем ответ что пункт меню создан
         """
-        menu_title = "Drinks"
-        menu_description="any description"
+        menu_title = 'Drinks'
+        menu_description = 'any description'
         data = {
-                "title": menu_title,
-                "description": menu_description,
-               }
-        response = await self.client.post("/menus", json=data)
+            'title': menu_title,
+            'description': menu_description,
+        }
+        response = await self.client.post('/menus', json=data)
         assert response.status_code == 201
-        pytest.id_menu = response.json()["id"]
+        pytest.id_menu = response.json()['id']
         pytest.menu_title = menu_title
-        pytest.menu_description= menu_description
-        assert response.json()["title"] == menu_title
-        assert response.json()["description"] == menu_description
-        assert response.json()["submenus_count"] == None
-        assert response.json()["dishes_count"] == None
+        pytest.menu_description = menu_description
+        assert response.json()['title'] == menu_title
+        assert response.json()['description'] == menu_description
+        assert response.json()['submenus_count'] == None  # noqa
+        assert response.json()['dishes_count'] == None  # noqa
 
-
-
-    @pytest.mark.asyncio(depends=["TestSubMenu::test_post_menu"])
-    async def test_get_sub_menus_in_menu(self):
+    @pytest.mark.asyncio(depends=['TestSubMenu::test_post_menu'])  # type: ignore
+    async def test_get_sub_menus_in_menu(self) -> None:
         """
         получаем список подменю в Меню и убеждаемся что подменю пустое
         """
-        response = await self.client.get(f"/menus/{pytest.id_menu}/submenus")
+        response = await self.client.get(f'/menus/{pytest.id_menu}/submenus')
         assert response.status_code == 200
-        assert response.json() ==[]
-        
+        assert response.json() == []
 
-
-    @pytest.mark.asyncio
-    async def test_post_sub_menu(self):
+    @pytest.mark.asyncio  # type: ignore
+    async def test_post_sub_menu(self) -> None:
         """
         создаем пункт подменю в созданном ранее меню
         """
-        sub_menu_title = "Cold"
-        sub_menu_description="any submenu description"
+        sub_menu_title = 'Cold'
+        sub_menu_description = 'any submenu description'
         data = {
-                "title": sub_menu_title,
-                "description": sub_menu_description,
-               }
-        response = await self.client.post(f"/menus/{pytest.id_menu}/submenus", json=data)
+            'title': sub_menu_title,
+            'description': sub_menu_description,
+        }
+        response = await self.client.post(f'/menus/{pytest.id_menu}/submenus', json=data)
         assert response.status_code == 201
-        pytest.id_sub_menu = response.json()["id"]
+        pytest.id_sub_menu = response.json()['id']
         pytest.sub_menu_title = sub_menu_title
-        pytest.sub_menu_description= sub_menu_description
-        assert response.json()["title"] == sub_menu_title
-        assert response.json()["description"] == sub_menu_description
-     
+        pytest.sub_menu_description = sub_menu_description
+        assert response.json()['title'] == sub_menu_title
+        assert response.json()['description'] == sub_menu_description
 
-
-    @pytest.mark.asyncio
-    async def test_get_sub_menu_not_empty(self):
+    @pytest.mark.asyncio  # type: ignore
+    async def test_get_sub_menu_not_empty(self) -> None:
         """
         проверяем что подменю не пустое
         """
-        response = await self.client.get(f"/menus/{pytest.id_menu}/submenus")
+        response = await self.client.get(f'/menus/{pytest.id_menu}/submenus')
         assert response.status_code == 200
         assert response.json() != []
 
-
-    @pytest.mark.asyncio()
-    async def test_get_sub_menu_by_id(self):
+    @pytest.mark.asyncio()  # type: ignore
+    async def test_get_sub_menu_by_id(self) -> None:
         """
         проверяем получение пункта подменю по id
         """
-        response = await self.client.get(f"/menus/{pytest.id_menu}/submenus/{pytest.id_sub_menu}")
+        response = await self.client.get(f'/menus/{pytest.id_menu}/submenus/{pytest.id_sub_menu}')
         assert response.status_code == 200
-        assert response.json()["title"] == pytest.sub_menu_title
-        assert response.json()["description"] == pytest.sub_menu_description
-        assert response.json()["dishes_count"] == 0
+        assert response.json()['title'] == pytest.sub_menu_title
+        assert response.json()['description'] == pytest.sub_menu_description
+        assert response.json()['dishes_count'] == 0
 
-
-
-    @pytest.mark.asyncio(depends=["TestSubMenu::test_post_menu"])
-    async def test_update_sub_menu_by_id(self):
+    @pytest.mark.asyncio(depends=['TestSubMenu::test_post_menu'])  # type: ignore
+    async def test_update_sub_menu_by_id(self) -> None:
         """
         1. обновляем ранее созданное подменю
         2. просматриваем только что созданное подменю, что title description изменился на новое
         """
-        new_sub_menu_title = "updated submenu Cold"
-        new_sub_menu_description="updated submenu description"
+        new_sub_menu_title = 'updated submenu Cold'
+        new_sub_menu_description = 'updated submenu description'
         data = {
-                "title": new_sub_menu_title,
-                "description": new_sub_menu_description,
-               }
-        response = await self.client.patch(f"/menus/{pytest.id_menu}/submenus/{pytest.id_sub_menu}", json=data)
+            'title': new_sub_menu_title,
+            'description': new_sub_menu_description,
+        }
+        response = await self.client.patch(f'/menus/{pytest.id_menu}/submenus/{pytest.id_sub_menu}', json=data)
         assert response.status_code == 200
-        assert response.json()["title"] == new_sub_menu_title
-        assert response.json()["description"] == new_sub_menu_description
-        response = await self.client.get(f"/menus/{pytest.id_menu}/submenus/{pytest.id_sub_menu}")
+        assert response.json()['title'] == new_sub_menu_title
+        assert response.json()['description'] == new_sub_menu_description
+        response = await self.client.get(f'/menus/{pytest.id_menu}/submenus/{pytest.id_sub_menu}')
         assert response.status_code == 200
-        assert response.json()["title"] == new_sub_menu_title
-        assert response.json()["description"] == new_sub_menu_description
-        assert response.json()["dishes_count"] == 0
+        assert response.json()['title'] == new_sub_menu_title
+        assert response.json()['description'] == new_sub_menu_description
+        assert response.json()['dishes_count'] == 0
 
-
-
-    @pytest.mark.asyncio(depends=["TestSubMenu::test_post_menu"])
-    async def test_delete_sub_menu_by_id(self):
+    @pytest.mark.asyncio(depends=['TestSubMenu::test_post_menu'])  # type: ignore
+    async def test_delete_sub_menu_by_id(self) -> None:
         """
         1. удаляем обновленное подменю
         2. просматриваем список подменю
         3. просматриваем определенное подменю и убеждаемся что его нет 404
         """
-        response = await self.client.delete(f"/menus/{pytest.id_menu}/submenus/{pytest.id_sub_menu}")
+        response = await self.client.delete(f'/menus/{pytest.id_menu}/submenus/{pytest.id_sub_menu}')
         assert response.status_code == 200
-        response = await self.client.get(f"/menus/{pytest.id_menu}/submenus")
+        response = await self.client.get(f'/menus/{pytest.id_menu}/submenus')
         assert response.status_code == 200
         assert response.json() == []
-        response = await self.client.get(f"/menus/{pytest.id_menu}/submenus/{pytest.id_sub_menu}")
+        response = await self.client.get(f'/menus/{pytest.id_menu}/submenus/{pytest.id_sub_menu}')
         assert response.status_code == 404
 
-    
-
-    @pytest.mark.asyncio(depends=["TestSubMenu::test_post_menu"])
-    async def test_delete_menu_by_id(self):
+    @pytest.mark.asyncio(depends=['TestSubMenu::test_post_menu'])  # type: ignore
+    async def test_delete_menu_by_id(self) -> None:
         """
         1. удаляем обновленное меню
         2. просматриваем список меню
         3. просматриваем определенное меню и убеждаемся что его нет 404
         """
-        response = await self.client.delete(f"/menus/{pytest.id_menu}")
+        response = await self.client.delete(f'/menus/{pytest.id_menu}')
         assert response.status_code == 200
-        response = await self.client.get(f"/menus")
+        response = await self.client.get('/menus')
         assert response.status_code == 200
         assert response.json() == []
-        response = await self.client.get(f"/menus/{pytest.id_menu}")
+        response = await self.client.get(f'/menus/{pytest.id_menu}')
         assert response.status_code == 404
